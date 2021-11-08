ef22cee1bf819efb80cf6abd2a1c84baf2624d68
Berninho <bernardrahani@ymail.com>
2021-11-08 16:59:29 +0100

    first webpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import Footer from './components/Footer.js';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="50" />
-        <h2>Header</h2>
-      </header>
-    <section>
+      <Header />
+      
+      <section>
         <h1>Contenu</h1>
-        <button type="button" class="btn btn-primary">Primary</button>
-    </section>
-    <footer>
-        <h2>Footer</h2>
-    </footer>
-    </div>
+
+      </section>
+      <Footer/>
+
+  </div>
   );
 }
 
