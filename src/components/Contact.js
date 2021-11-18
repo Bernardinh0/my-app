e2b435bf4c7ab0384b0919f7e08affb816b52015
Rahanitriniaina <bernardrahani@ymail.com>
2021-11-18 11:56:45 +0100

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
         <div className="row"><h1>Contact</h1></div>
         <div className="row">
           <div className="col-12 col-md-6">
-          <img className="img-fluid" src="https://64.media.tumblr.com/a720071d847ae1427133eb8073a9f872/tumblr_onydp7n5aV1qbfzero1_500.gifv" alt="Odile de ray" />
+          <img className="img-fluid" src="https://giffiles.alphacoders.com/166/166658.gif" alt="Batman" />
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d45656.41178938134!2d1.1848141574795723!3d44.340250037452876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12ab80990bc906fd%3A0x14041eaa5e900cff!2sMontcuq%2C%2046800%20Montcuq-en-Quercy-Blanc!5e0!3m2!1sfr!2sfr!4v1636543109709!5m2!1sfr!2sfr" width="100%" height="450" loading="lazy"></iframe>
             </div>
           <div className="col-12 col-md-6">
